69643b20d4bf0e9f9c27a197f7e9238ff0c719fd
Camilla Westin <camilla@webstin.se>
2018-12-29 01:09:27 +0100

    Styling background

--- src/js/nextBus.js
+++ src/js/nextBus.js
@@ -5,7 +5,6 @@ function updateBus() {
     $.ajax({
         type:"GET",
         url:"https://api.sl.se/api2/realtimedeparturesV4.json?key=32a8e47d03c2482e8c3a31074afd5f53&siteid=4124&timewindow=20",
-        dataType: 'json',
         success:function(data) {
             var busDepartures = data.ResponseData.Buses;
             $.each(busDepartures, function() {
